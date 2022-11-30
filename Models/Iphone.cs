ed962893fb5478cb2cc826fbd4e856e04663cd0e
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-11-30 17:16:03 -0300

    feat: challenge finished

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class IPhone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public IPhone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria) { }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp}...");
+            Console.WriteLine($"{nomeApp} instalado com sucesso!");
+        }
     }
 }
